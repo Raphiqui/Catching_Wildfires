c36aa29ca9ebec8d7cf9f91bbf2db75486f68a18
Norsse <raphael.pastre@gmail.com>
2019-10-07 15:44:31 +0200

    setup libraries

---
+++ app.py
@@ -0,0 +1,8 @@
+import json
+import time
+import pprint
+import telepot
+import requests
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+
+print(f'hello world')
