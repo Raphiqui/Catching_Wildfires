cd29ec76a0998368d54c715ece4deccfc7f28221
Norsse <raphael.pastre@gmail.com>
2019-10-07 15:53:33 +0200

    add conf file and parsing to fetch bot token

--- app.py
+++ app.py
@@ -5,4 +5,21 @@ import telepot
 import requests
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 
-print(f'hello world')
+
+def fetch_conf():
+    """
+    Parses the configuration file to fetch the bot's token
+    :return: token as string from telegram bot application
+    """
+    with open('conf.json') as json_data_file:
+        data = json.load(json_data_file)
+    return data["bot_token"]
+
+
+if __name__ == '__main__':
+    bot_token = fetch_conf()
+    bot = telepot.Bot(bot_token)
+
+    print('Listening ...')
+    while True:
+        time.sleep(1)
